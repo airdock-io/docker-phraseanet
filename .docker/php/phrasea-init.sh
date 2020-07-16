06931d3d4b2331e444e20feece79f7333e924df1
purplebabar <lalung.alexandre@gmail.com>
2020-07-16 17:42:19 +0200

    change rights on folder

--- .docker/php/phrasea-init.sh
+++ .docker/php/phrasea-init.sh
@@ -3,7 +3,7 @@
 bin/console compile:configuration >> /dev/stdout
 bin/developer orm:generate-proxies >> /dev/stdout
 bin/console searchengine:index:create >> /dev/stdout
-chmod -R 777 logs cache config tmp www/custom datas
+chmod -R 777 logs cache config tmp www datas
 [ ! -d "datas/phrasea_data/documents" ] && mkdir -p -m 777 datas/phrasea_data/documents
 
 if [ "$RUN_AS_WORKER" = true ]
